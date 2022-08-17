357d51d738d9221cd5943d106914e0ba3b84723c
majakwani <anasjakwani72@gmail.com>
2022-08-17 15:06:33 +0500

    Wrote Scripts for creating Players objects and adding them in an array

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+function createPlayers(number){
+    let playerName = prompt(`Enter name for Player #${number} : `);
+    let playerNumber = number + 1;
+    let playerMark;
+
+    if(playerNumber == 1){
+        playerMark = "X"
+    }
+    else if (playerNumber == 2){
+        playerMark = "O"
+    }
+
+    gameBoardModule.players.push({ playerName, playerNumber, playerMark })
+    return 
+}
+
+let gameBoardModule = (function(){
+    let gameBoard = [];
+    let players = [];
+
+    return { players }
+})();
+
+function gameController(){
+    for(let i = 0; i < 2; i++){
+        createPlayers(i);
+    }
+}
