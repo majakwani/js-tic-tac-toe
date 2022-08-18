b4f01288d8ef3e5ae26f9d9f1228392befa4035d
majakwani <anasjakwani72@gmail.com>
2022-08-18 17:57:26 +0500

    Wrote every script except for when match is drawn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let Turn = false;
 const circleClass = "o";
 const xClass = "x";
 const message = document.querySelector('.messageDisplay');
+const buttons = document.querySelector('.buttons');
 
 // Creates player object and append it in players array
 function createPlayers(name, number){
@@ -18,40 +19,48 @@ function createPlayers(name, number){
 
 // 
 let gameBoardModule = (function(){
-    let gameBoard = [];
+    let gameBoardX = [];
+    let gameBoardO = [];
     let players = [];
-    return { gameBoard , players }
+    return { players, gameBoardO, gameBoardX }
 })();
 
-let displayController = (function(){
-
-})();
-
-// Reads gameboard cells and store them in gameboard array
-let readingGameBoard = ( ()=>{
-    let i = 1;
-    let gameboard = document.querySelectorAll('.gameboard-cell');
-    gameboard.forEach(element => {
-        element.setAttribute("id", i);
-        i = i + 1;
-        gameBoardModule.gameBoard.push(element)
-    });
-})();
+// Reads gameboard cells and user mark and store them in an array og objects
+function readingGameBoard(mark, index){
+    if(mark == "x"){
+        gameBoardModule.gameBoardX.push(index)
+    }
+    else{
+        gameBoardModule.gameBoardO.push(index)
+    }
+}    
 
 function placeMark(cell, mark){
     cell.classList.add(mark)
 }
 
 function switchTurns(index){
+    let name = "";
+    let module = Turn ? gameBoardModule.gameBoardO : gameBoardModule.gameBoardX;
+    if(index == "0"){
+        name = gameBoardModule.players[1].name;
+    }
+    else if (index == "1"){
+        name = gameBoardModule.players[0].name;
+    }
+    checkForWin(module, name)
     message.innerHTML = `${gameBoardModule.players[index].name}'s Turn, Mark = ${gameBoardModule.players[index].playerMark}`;
     Turn = !Turn
 }
 
 function handleEvent(e){
     let cell = e.target;
+    let cellid = e.target.id;
     let currentClass = Turn ? circleClass : xClass;
     let number = Turn ? "0" : "1";
     placeMark(cell, currentClass)
+    readingGameBoard(currentClass, cellid)
+
     switchTurns(number)
 }
 
@@ -68,7 +77,6 @@ function addNames(){
     btn.remove();
     const playerOne = document.querySelector('#player1').value;
     const playerTwo = document.querySelector('#player2').value;
-    
     // Checks for empty input fields.
     if(playerOne == "" || playerTwo == ""){
         alert("Empty Input Fields");
@@ -104,11 +112,37 @@ function formCreation(){
     body.append(form);
 }
 
-function clearBoard(){
-    const cell = document.querySelectorAll('.gameboard-cell');
-    cell.forEach(element => {
-        element.classList.remove("x");
-        element.classList.remove("o");
-        gameStart() 
+function newGame(){
+    location.reload();
+}
+
+function checkForWin(turn, name){
+    let winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    winningCombinations.forEach(element => {
+        let count = 0;
+        element.forEach(value => {
+            turn.forEach(check => {
+                if(value == check){
+                    count = count + 1;
+                }
+            });
+            if(count == 3){
+                const winningMessage = document.createElement("div");
+                winningMessage.classList.add("endMessage" , "flex");
+                winningMessage.innerHTML = `        <p>${name} Won</p>
+                <button type="button" onclick = "newGame()" >Play Again</button>`
+                document.body.append(winningMessage);
+            }
+        });
     });
+    console.log(gameBoardModule.gameBoardO, gameBoardModule.gameBoardX)
 }
